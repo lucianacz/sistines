4222539736c382ac1219ae9e8d317f0f82df07ce
Luciana Czikk <lucianaczikk@gmail.com>
2021-10-26 11:15:47 -0600

    Rewards page

--- assets/custom.js
+++ assets/custom.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    var $popUpClose = $('.popup-modal-close');
-    var $popUp = $('.popup-modal');
-      if($popUp.hasClass('completed')) {
-        window.setTimeout(function () {
-        $popUpClose.appendTo( $('.popup-modal') );
-        }, 100);
-      } else{
-        $popUpClose.appendTo( $('.off-canvas--main-content') );
-      }
+  var $popUpClose = $('.popup-modal-close');
+  var $popUp = $('.popup-modal');
+  if ($popUp.hasClass('completed')) {
+    window.setTimeout(function () {
+      $popUpClose.appendTo($('.popup-modal'));
+    }, 100);
+  } else {
+    $popUpClose.appendTo($('.off-canvas--main-content'));
+  }
 });
